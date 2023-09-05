61274075d00e94c1cc7701fc22376adfa8dc08ac
zxuhan7 <zxuhan7@gmail.com>
2023-09-05 21:06:33 +0200

    completed first part of lab

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,14 @@
+public class HelloNumber {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            sum = sum + x;
+            System.out.print(sum + " ");
+            x = x + 1;
+        }
+    }
+}
+
+
+
