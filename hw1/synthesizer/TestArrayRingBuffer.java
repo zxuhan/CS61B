539d85fdd1889c8977258701092255c59e6ea5cf
zxuhan7 <zxuhan7@gmail.com>
2023-10-06 11:57:31 +0200

    completed hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,31 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(10);
+        arb.enqueue(20);
+        arb.enqueue(30);
+        int actural1 = arb.peek();
+        int expected1 = 10;
+        assertEquals(expected1, actural1);
+
+        int actural2 = arb.dequeue();
+        int expected2 = 10;
+        assertEquals(expected2, actural2);
+
+        int actural3 = arb.peek();
+        int expected3 = 20;
+        assertEquals(expected3, actural3);
+
+        arb.enqueue(40);
+        arb.enqueue(40);
+        arb.enqueue(40);
+        arb.enqueue(40);
+        arb.enqueue(40);
+        arb.enqueue(40);
+        arb.enqueue(40);
+        arb.enqueue(40);
+
     }
 
     /** Calls tests for ArrayRingBuffer. */
