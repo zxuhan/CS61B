03806363a2a8fa2cab4952f482a9a2c75f22d9b7
zxuhan7 <zxuhan7@gmail.com>
2023-12-22 23:49:28 +0100

    fixed error in calcForceBY

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,7 +6,7 @@ public class Planet {
     public double mass;
     public String imgFileName;
     
-    public static final double GRAVITY = 6.67e-11;
+    private static final double GRAVITY = 6.67e-11;
 
     public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
         this.xxPos = xxPos;
@@ -48,7 +48,7 @@ public class Planet {
     }
 
     public double calcForceExertedByY(Planet p) {
-        double yyDistance = p.yyPos - this.xxPos;
+        double yyDistance = p.yyPos - this.yyPos;
         double distance = calcDistance(p);
         double forceExerted = calcForceExertedBy(p);
         double forceExertedByY = forceExerted * yyDistance / distance;
