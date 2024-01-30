2b5b4b9deca73feee0e100ffbee73bc71f704f5a
zxuhan7 <zxuhan7@gmail.com>
2024-01-30 16:58:55 +0100

    completed lab11 except for A* part

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int v;
+    private Queue<Integer> fringe;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        s = maze.xyTo1D(sourceX, sourceY);
+        v = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        fringe = new Queue<>();
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        fringe.enqueue(s);
+        marked[s] = true;
+        
+        while (!fringe.isEmpty()) {
+            int delVertex = fringe.dequeue();
+
+            if (delVertex == v) {
+                return;
+            }
+
+            for (int i : maze.adj(delVertex)) {
+                if (marked[i] == false) {
+                    fringe.enqueue(i);
+                    marked[i] = true;
+                    edgeTo[i] = delVertex;
+                    distTo[i] = distTo[delVertex] + 1;
+                    announce();
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,60 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private int[] edgeToCopy;
+    private boolean isCycle;
+    
     public MazeCycles(Maze m) {
         super(m);
+        edgeToCopy = new int[maze.V()];
+        for (int i = 0; i < maze.V(); i += 1) {
+            edgeToCopy[i] = Integer.MAX_VALUE;
+        }
+        edgeToCopy[0] = 0;
+        isCycle = false;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        // Your code here!
+        
+        dfs(0);
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+
+        if (isCycle) {
+            return;
+        }
+
+        for (int i : maze.adj(v)) {
+            if (marked[i] == true && edgeToCopy[v] != i) {
+                isCycle = true;
+                int prev = edgeToCopy[v];
+                edgeTo[v] = prev;
+                while (prev != i) {
+                    edgeTo[prev] = edgeToCopy[prev];
+                    prev = edgeToCopy[prev];
+                }
+                edgeTo[i] = v;
+                announce();
+                return;
+            }
+            if (marked[i] == false) {
+
+                marked[i] = true;
+                edgeToCopy[i] = v;
+                announce();
+
+                dfs(i);
+
+                if (isCycle) {
+                    return;
+                }
+            }
+        }
+    }
 }
 
